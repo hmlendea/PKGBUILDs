8ff74ffc4659cdcc36c7a5427166a9fa1eca172b
hmlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2020-02-22 12:04:54 +0200

    Skip rebuilding unchanged packages

--- makepkgs.sh
+++ makepkgs.sh
@@ -10,29 +10,48 @@ cd "$DIR"
 
 REPONAME="hmlendea"
 
-echo "Cleaning the current repository database"
-rm -rf $DIR/repo/*
+if [ -f "${DIR}/repo/${REPONAME}.db" ]; then
+    echo "Cleaning the current repository database"
+
+    rm "${DIR}/repo/${REPONAME}.db"
+    rm "${DIR}/repo/${REPONAME}.db.tar.gz"
+    rm "${DIR}/repo/${REPONAME}.files"
+    rm "${DIR}/repo/${REPONAME}.files.tar.gz"
+fi
 
 for PKGDIR in pkg/*; do
     if [ ! -f "$PKGDIR/PKGBUILD" ]; then
         continue
     fi
 
-    cd $PKGDIR
-    echo "Making package in $PKGDIR..."
-    makepkg -Csrf --noconfirm
+    PKGNAME=$(cat "${PKGDIR}/PKGBUILD" | grep "^pkgname=" | awk -F'=' '{print $2}')
+    PKGVER=$(cat "${PKGDIR}/PKGBUILD" | grep "^pkgver=" | awk -F'=' '{print $2}')
+    PKGREL=$(cat "${PKGDIR}/PKGBUILD" | grep "^pkgrel=" | awk -F'=' '{print $2}')
 
-    PKGNAME=$(cat PKGBUILD | grep "^pkgname=" | awk -F'=' '{print $2}')
-    PKGVER=$(cat PKGBUILD | grep "^pkgver=" | awk -F'=' '{print $2}')
-    PKGREL=$(cat PKGBUILD | grep "^pkgrel=" | awk -F'=' '{print $2}')
+    PKGID="${PKGNAME}-${PKGVER}-${PKGREL}"
 
-    for PKGFILE in $PKGNAME-$PKGVER-$PKGREL-*.pkg.tar.xz; do
-        echo "Registering $PKGFILE"
-        cp $PKGFILE "$DIR/repo/"
+    # TODO: Support other architectures
+    if [ ! -f "repo/${PKGID}-any.pkg.tar.xz" ] &&
+       [ ! -f "repo/${PKGID}-x86_64.pkg.tar.xz" ]; then
+        cd ${PKGDIR}
 
-        cd $DIR
-        repo-add "repo/$REPONAME.db.tar.gz" "repo/$PKGFILE"
-    done
+        echo "Making ${PKGID}..."
+        makepkg -Csrf --noconfirm
+
+        echo "Removing the old versions of ${PKGNAME}..."
+        rm ${DIR}/repo/${PKGNAME}-*.pkg.tar.xz
+
+        for PKGFILE in ${PKGID}-*.pkg.tar.xz; do
+            cp ${PKGFILE} "$DIR/repo/"
+        done
+    fi
+done
+
+cd ${DIR}
+
+for PKGFILE in repo/*.pkg.tar.xz; do
+    echo "Registering ${PKGFILE}"
+    repo-add "repo/${REPONAME}.db.tar.gz" "${PKGFILE}"
 done
 
 rm "$DIR/repo/$REPONAME.db"
